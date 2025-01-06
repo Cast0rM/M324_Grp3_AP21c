01a8269dae4bcdfe7faa883193823401beaf1e7c
kusibehan12 <kusibehan@outlook.com>
2025-01-06 14:41:29 +0100

    base schemas for the requests

--- Code/backend/app/schemas.py
+++ Code/backend/app/schemas.py
@@ -1,23 +1,19 @@
 from pydantic import BaseModel
+from datetime import date, datetime
 
 
 class Albumbase(BaseModel):
     title: str
-    release_date = str
-    band_id = int
-    label = str
-    price = float
-    created_at = str
+    release_date: date
+    band_id: int
+    label: str
+    price: float
+    created_at: datetime
 
-# class BandBase(BaseModel):
-#     title: str
-#     label: str
-
-# class ItemCreate(BandBase):
-#     pass
-
-# class Item(BandBase):
-#     id: int
-
-#     class Config:
-#         orm_mode = True
+class BandBase(BaseModel):
+    name: str
+    genre: str
+    founding_date: date
+    members_count: int
+    disbanded_date: date
+    created_at: datetime
