436226640004a9e2f605fe6dd5a10c9cc5791f12
kusibehan12 <kusibehan@outlook.com>
2025-01-09 08:29:55 +0100

    Post Bands

--- Code/backend/app/main.py
+++ Code/backend/app/main.py
@@ -7,6 +7,7 @@ from . import models
 from typing import List
 from . import schemas
 from . import CRUD
+from pydantic import ValidationError
 
 
 def get_db_album():
@@ -35,7 +36,7 @@ def get_albums(db: Session = Depends(get_db_album)):
 
 @app.get(
     "/bands/",
-    response_model=List[schemas.BandBase],
+    response_model=List[schemas.ReadBand],
     responses={
         404: {"model": schemas.MessageNotFound, "description": "The item was not found"}
     },
@@ -51,26 +52,42 @@ def get_bands(db: Session = Depends(get_db_bands)):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Database Server Error",
         ) from e
-        
+
+
 @app.get(
     "/bands/{band_id}",
-    response_model=schemas.BandBase,
+    response_model=schemas.ReadBand,
     responses={
         404: {"model": schemas.MessageNotFound, "description": "The item was not found"}
     },
 )
-
 def get_bands_id(band_id: int, db: Session = Depends(get_db_bands)):
     try:
         band = CRUD.get_band(band_id, db=db)
         if not band:
-            return JSONResponse(status_code=404, content={"message": "Keine Band gefunden"})
+            return JSONResponse(
+                status_code=404, content={"message": "Keine Band gefunden"}
+            )
         return band
     except SQLAlchemyError as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Database Server Error",
         ) from e
+
+
+@app.post("/bands/", response_model=schemas.BandBase)
+def post_bands(band: schemas.BandCreate, db: Session = Depends(get_db_bands)):
+    try:
+        new_band = CRUD.post_band(db=db, band=band)
+        return new_band
+    except SQLAlchemyError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error",
+        ) from e
+    except ValidationError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
 
 # docker-compose down
--- Code/backend/app/models.py
+++ Code/backend/app/models.py
@@ -1,7 +1,18 @@
-from sqlalchemy import Column, Integer, String, Date, DECIMAL, TIMESTAMP, ForeignKey, DateTime, CheckConstraint
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Date,
+    DECIMAL,
+    TIMESTAMP,
+    ForeignKey,
+    DateTime,
+    CheckConstraint,
+)
 from sqlalchemy.sql import func
 from sqlalchemy.sql.functions import now
 from .database import Album_Base, Bands_Base
+
 
 class Album(Album_Base):
     __tablename__ = "albums"
@@ -13,26 +24,27 @@ class Album(Album_Base):
     label = Column(String(100), nullable=False)
     price = Column(DECIMAL(10, 2), nullable=False)
     created_at = Column(TIMESTAMP, server_default=func.current_timestamp())
-    
+
+
 class Bands(Bands_Base):
     __tablename__ = "bands"
     band_id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(100), nullable=False) 
-    genre = Column(String(50), nullable=False) 
+    name = Column(String(100), nullable=False)
+    genre = Column(String(50), nullable=False)
     founding_date = Column(Date, nullable=False)
-    members_count= Column(Integer, nullable=False)
-    disbanded_date = Column(String(10), default=None)
-    created_at = Column(DateTime, default= now())
-    
+    members_count = Column(Integer, nullable=False)
+    disbanded_date = Column(String(10), default=None, nullable=True)
+    created_at = Column(DateTime, default=now())
+
     # __table_args__ = (
     #     CheckConstraint(
     #         "disbanded_date IS NULL OR disbanded_date > founding_date",
     #         name="check_disbanded_after_founding"
     #     ),
     # )
-    
-    
+
+
 # class Bands(Bands_Base)
-    
-    
+
+
 # Base.metadata.create_all()
